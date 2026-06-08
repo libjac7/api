763a71a169682486d8e26c4a2996d9fe0b617e0a
libjac7 <117264106+libjac7@users.noreply.github.com>
2026-06-08 02:59:21 -0600

    Add route to assign a boss to an employee

--- src/routes/emp.routes.js
+++ src/routes/emp.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { registrarEmpleado, obtenerInformacionEmpleados } from '../controllers/emp.controller.js';
+import { registrarEmpleado, obtenerInformacionEmpleados, asignarJefeAEmpleado } from '../controllers/emp.controller.js';
 import { verificarJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.post('/', verificarJWT, registrarEmpleado);
 router.post('/inf-empleado', verificarJWT, obtenerInformacionEmpleados);
+router.post('/asignar-jefe', verificarJWT, asignarJefeAEmpleado);
 
 export default router;
